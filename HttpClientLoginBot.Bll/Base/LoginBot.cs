24c9a80648b5a29414678de6c084451727f2c4fd
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2019-04-17 00:22:14 +0200

    Removed commented code from login bot

--- HttpClientLoginBot.Bll/Base/LoginBot.cs
+++ HttpClientLoginBot.Bll/Base/LoginBot.cs
@@ -34,39 +34,6 @@ namespace HttpClientLoginBot.Bll.Base {
             UseProxy = true;
         }
 
-        //public virtual void LoadCredentials (string seperator) {
-        //    var credentialsList = new List<LoginCredential> ();
-        //    using (var reader = new StreamReader (_pathToCredentials)) {
-        //        var line = reader.ReadLine ();
-        //        var lineSplitResult = line.Split (seperator);
-        //        var username = lineSplitResult[0];
-        //        var password = lineSplitResult[1];
-        //        var credential = new LoginCredential (username, password);
-                
-        //        credential.RequestBody = username + password;
-        //        credential.Url = _url;
-
-        //        credentialsList.Add (credential);
-        //    }
-        //    _credentials = credentialsList;
-        //}
-
-        //public virtual void LoadProxyList (string seperator) {
-        //    var proxyList = new List<LoginProxy> ();
-        //    using (var reader = new StreamReader (_pathoToProxyFileList)) {
-        //        while (!reader.EndOfStream) {
-        //            var line = reader.ReadLine ();
-        //            var lineSplitResult = line.Split (seperator);
-        //            var host = lineSplitResult[0];
-        //            var port = lineSplitResult[1];
-        //            var proxy = new LoginProxy (host, port);
-        //            proxyList.Add (proxy);
-        //        }
-        //    }
-
-           
-        //}
-
         public async void Run () {
             foreach (var credential in _credentials) {
 
